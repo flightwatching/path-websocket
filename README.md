23920fd3715e6d69413e0d9f68f9d9891a5f31f7
dao hodac <dao.hodac@gmail.com>
2016-03-29 18:50:58 +0200

    init repo

---
+++ README.md
@@ -0,0 +1,14 @@
+# gyro-websocket
+send accelerometer info to a websocket concentrator to be used in WILCO (demo purpose)
+
+Installation
+=======
+
+```npm install express@4.10.2```
+
+```npm install socket.io```
+
+
+Run
+=====
+```node index.js```
---
+++ server.js
@@ -0,0 +1,45 @@
+
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+var argv = require('yargs').argv;
+
+
+var positions;
+var currentI = 0;
+
+fs.readFile(argv.f, 'utf8', function (err, data) {
+  if (err) throw err;
+  positions = JSON.parse(data);
+  setInterval(function() {
+    console.log(currentI);
+    console.log(positions[currentI]);
+    io.emit('positions', positions[currentI]);
+    currentI=(currentI+1)%positions.length;
+  }, 1000);
+});
+
+
+
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+  socket.on('touch', function(msg){
+    io.emit('touch', msg);
+  });
+  // socket.on('devicemotion', function(msg){
+  //   console.log(JSON.parse(msg));
+  // });
+  socket.on('deviceorientation', function(msg){
+    io.emit('deviceorientation', msg);
+  });
+});
+
+
+http.listen(3001, function(){
+  console.log('listening on *:3001');
+});
